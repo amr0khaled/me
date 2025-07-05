386fd58733ca4fc76d1f4c56bf0170ea9a8c55ad
Amr Khaled <redwolf4325@gmail.com>
2025-07-05 23:41:49 +0300

    Cleaning before working

--- src/layout/landing-page.tsx
+++ src/layout/landing-page.tsx
@@ -1,12 +1,8 @@
-import About from "@/sections/about"
-import { useIsMobile } from "@/hooks/is-mobile"
 
 
 export default function Content() {
-  const isMobile = useIsMobile()
   return (
     <main className='flex justify-center items-center min-h-[90vh]'>
-      <About />
     </main>
   )
 }
