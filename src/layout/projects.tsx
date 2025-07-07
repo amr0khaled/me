d17e797b2d4cb222efbcd7fab86bd022aafee8d6
Amr Khaled <redwolf4325@gmail.com>
2025-07-07 23:14:12 +0300

    Build the layout of projects section

---
+++ src/layout/projects.tsx
@@ -0,0 +1,37 @@
+import '@/style/layout/projects.css';
+import { useState } from 'react';
+
+type Project = {
+  title: string
+  images: string[]
+  description: string
+}
+
+export default function Projects() {
+  const [project, setProject] = useState<Project>({
+    title: "Test",
+    images: [],
+    description: 'description'
+  });
+  const [projects, setProjects] = useState<Project[]>([])
+  return <section className='projects-section'>
+    <header>
+      <div className='divider'></div>
+      <h1 className='title'>~/Projects/{project.title}</h1>
+      <div className='divider'></div>
+    </header>
+    <main className='body'>
+      <section className='description-section'>
+        <p className='description'>
+          {project.description}
+        </p>
+      </section>
+      <div className='divider-vertical'></div>
+      <section className='image-section'>
+        <div className='image-frame'>
+          <img />
+        </div>
+      </section>
+    </main>
+  </section>
+}
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,13 @@
 import Contact from "@/layout/contact"
 import Header from "@/layout/header"
+import Projects from "@/layout/projects"
 
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Projects />
       <Contact />
     </>
   )
