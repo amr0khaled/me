3b48627cf8132d92c95ea9fd7111099aeccabd21
Amr Khaled <redwolf4325@gmail.com>
2025-07-11 02:15:43 +0300

    Finished the 1st prototype of landing section

--- src/layout/landing-page.tsx
+++ src/layout/landing-page.tsx
@@ -1,8 +1,52 @@
+import { Skeleton } from '@/components/ui/skeleton';
+import '@/style/layout/landing.css';
+import { useEffect, useRef, useState } from 'react';
 
-
-export default function Content() {
+export default function Landing() {
+  const img1 = useRef<HTMLDivElement>(null)
+  const img2 = useRef<HTMLDivElement>(null)
+  const [{
+    width1,
+    width2,
+    height1,
+    height2
+  }, setDim] = useState({
+    width1: 0,
+    width2: 0,
+    height1: 0,
+    height2: 0,
+  })
+  useEffect(() => {
+    const im1 = img1.current
+    const im2 = img2.current
+    if (im1 && im2) {
+      setDim(e => {
+        return {
+          ...e,
+          width1: im1.offsetWidth,
+          width2: im2.offsetWidth,
+          height1: im1.offsetHeight,
+          height2: im2.offsetHeight,
+        }
+      })
+    }
+  }, [])
   return (
-    <main className='flex justify-center items-center min-h-[90vh]'>
-    </main>
+    <section className='landing'>
+      <div className='img-container'>
+        <Skeleton ref={img1} className='aspect-[4/5] w-[264px] rounded-none ' />
+        <div style={{ width: width1, height: height1 }} className={`shadow-box -z-99`}></div>
+      </div>
+      <span className='name'>
+        Amr Khaled
+      </span>
+      <p className='paragraph'>
+        This is a paragraph
+      </p>
+      <div className='img-container'>
+        <Skeleton ref={img2} className='aspect-[5/4] h-[284px] rounded-none ' />
+        <div style={{ width: width2, height: height2 }} className={`shadow-box -z-99`}></div>
+      </div>
+    </section>
   )
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,6 @@
 import Contact from "@/layout/contact"
 import Header from "@/layout/header"
+import Landing from "@/layout/landing-page"
 import Projects from "@/layout/projects"
 import SkillsSection from "@/layout/skills"
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <>
       <Header />
+      <Landing />
       <SkillsSection />
       <Projects />
       <Contact />
