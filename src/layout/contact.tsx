0bffc1a8bcd50b36366f581e1bfda8c6226d083a
Amr Khaled <redwolf4325@gmail.com>
2025-07-10 23:06:15 +0300

    Fixed coloring issue with switching themes

--- src/layout/contact.tsx
+++ src/layout/contact.tsx
@@ -3,7 +3,9 @@ import "@/style/layout/contact.css"
 
 export default function Contact() {
   return <section className='contact-section'>
-    <h1>Type "contact"</h1>
-    <TerminalPane className='w-[calc(100%-40px)] bg-transparent mb-12 font-[JetBrains_MonoNerd] font-light' />
+    <main className='contact-container'>
+      <h1>Type "contact"</h1>
+      <TerminalPane className='w-[calc(100%-40px)] bg-transparent mb-12 font-[JetBrains_MonoNerd] font-light' />
+    </main>
   </section>
 }
